8f74c4932805f5c163162ac8913c6a4765afb637
Dmitriy_Bogat <bogat.dv@mail.ru>
2022-07-12 18:25:49 +0300

    Мозговыносительное задание под звездочкой, но вроде работает. С байтами париться уже не стал - на понимание интерфеса это не влияет, тупо присвоил значение

--- lab7/main.go
+++ lab7/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"strings"
 )
 
 type MyWreader []byte
@@ -13,16 +12,10 @@ func (M *MyWreader) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
-func (M *MyWreader) Read(p []byte) (int, error) {
-	s := make([]byte, len(*M))
-	buf := strings.NewReader(string(*M))
-	n, err := buf.Read(s)
-	fmt.Println(string(s))
-	fmt.Printf("Прочитано %v байт", n)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return len(p), nil
+func (M *MyWreader) Read(buf []byte) (int, error) {
+	buf = *M
+	fmt.Println(string(buf))
+	return len(buf), io.EOF
 }
 func main() {
 
